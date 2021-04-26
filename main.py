365a7b6ba82a31d2f000698cb17c39ccb46d147b
Зубаль Александр <zubal04@mail.ru>
2021-04-26 06:08:04 +0500

    А Саня продуктивно работал, да да(неважно что единственный раз за вечность)

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from data import db_session
 from data.jobs import Job
 from data.users import User
 from forms.user import RegisterForm
+from forms.login import LoginForm
+from forms.job import JobForm
+from flask_login import LoginManager, login_user, login_required
+from flask_login import logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
@@ -43,6 +55,55 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/addjob', methods=['GET', 'POST'])
+def add_job():
+    form = JobForm()
+    if form.validate_on_submit():
+        if not form.team_leader.data.isdigit():
+            return render_template('job_add.html', title='Регистрация',
+                                   form=form,
+                                   message="Id of leader must be integer")
+        if not form.work_size.data.isdigit():
+            return render_template('job_add.html', title='Регистрация',
+                                   form=form,
+                                   message="Work size must be integer")
+        db_sess = db_session.create_session()
+        job = Job(
+            job=form.job.data,
+            team_leader=form.team_leader.data,
+            work_size=form.work_size.data,
+            collaborators=form.collaborators.data,
+            is_finished=form.is_finished.data,
+        )
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('job_add.html', title='Добавление работы', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        user.set_password("123")
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
